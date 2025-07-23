4c3fd54a246bc6299a41c4db9456ea624c8d55b1
Ialbertine <a.ingabire1@alustudent.com>
2025-07-23 22:41:25 +0200

    terraform configuration and also azure deployment

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -8,6 +8,9 @@ const {
 } = require("../controllers/authController");
 
 // Public routes
+router.get('/', (req, res) => {
+  res.json({ message: 'Auth routes are working' });
+});
 router.post("/register", register);
 router.post("/login", login);
 
--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 3.80"
     }
   }
 }
@@ -12,26 +12,13 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "bookhub" {
-  name     = "bookhub-resources"
-  location = "East US"
-}
-
-module "database" {
-  source     = "./modules/database"
-  location   = azurerm_resource_group.bookhub.location
-  rg_name    = azurerm_resource_group.bookhub.name
-}
-
-module "container_registry" {
-  source     = "./modules/container_registry"
-  location   = azurerm_resource_group.bookhub.location
-  rg_name    = azurerm_resource_group.bookhub.name
+  name     = "bookhub_devops"
+  location = "centralindia" 
 }
 
 module "app_runner" {
-  source          = "./modules/app_runner"
-  location        = azurerm_resource_group.bookhub.location
-  rg_name         = azurerm_resource_group.bookhub.name
-  acr_name        = module.container_registry.acr_name
-  database_url    = module.database.connection_string
+  source    = "./modules/app_runner"
+  location  = azurerm_resource_group.bookhub.location
+  rg_name   = azurerm_resource_group.bookhub.name
+  mongo_uri = var.mongo_uri  # Make sure to pass this as TF_VAR_mongo_uri
 }
